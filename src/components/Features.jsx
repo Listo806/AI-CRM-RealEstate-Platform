137e9b68a753f4661d22de2a0f8f40d99e8cdc57
Listo806 <listoqasa@proton.me>
2025-10-20 23:07:53 -0500

    Create Features.jsx

---
+++ src/components/Features.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Features() {
+  return (
+    <section>
+      <h3>Key Features</h3>
+      <ul>
+        <li>Smart lead management</li>
+        <li>Automated alerts and notifications</li>
+        <li>Team collaboration and analytics</li>
+      </ul>
+    </section>
+  );
+}
