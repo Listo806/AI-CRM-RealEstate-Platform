c5b8bc9ab34b0fbd6b2d360db62540668d29118b
Listo806 <listoqasa@proton.me>
2025-10-20 23:10:24 -0500

    Create Testimonials.jsx

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Testimonials() {
+  return (
+    <section>
+      <h3>Testimonials</h3>
+      <p>"This AI CRM transformed our workflow!" - Happy Customer</p>
+    </section>
+  );
+}
