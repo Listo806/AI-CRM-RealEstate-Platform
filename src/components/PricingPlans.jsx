3aee80b28fa5aa2343a13095ae833943fd34bd45
Listo806 <listoqasa@proton.me>
2025-10-20 23:08:35 -0500

    Create PricingPlans.jsx

---
+++ src/components/PricingPlans.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PricingPlans() {
+  return (
+    <section>
+      <h3>Pricing Plans</h3>
+      <div>
+        <div>Pro - $49.99/mo</div>
+        <div>Pro Plus - $149.99/mo (Includes Smart CRM)</div>
+        <div>Pro Max - $249.99/mo</div>
+      </div>
+    </section>
+  );
+}
